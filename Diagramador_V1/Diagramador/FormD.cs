6c45a62a73fce0c135cc594c5ba95b640f7ae3cb
Agustin-Basilio-Matijasevich <agustinbasiliomatijasevich@gmail.com>
2022-06-17 20:51:09 -0300

    Estructuracion Codigo de la Forma D

--- Diagramador_V1/Diagramador/FormD.cs
+++ Diagramador_V1/Diagramador/FormD.cs
@@ -12,10 +12,25 @@ namespace Diagramador
 {
     public partial class FormD : Form
     {
+        #region Variables y Constantes
+
+        #endregion
+
+        #region Load
         public FormD()
         {
             InitializeComponent();
             this.CenterToScreen();
         }
+        #endregion
+
+        #region Metodos
+
+        #endregion
+
+        #region Handlers
+
+        #endregion
+
     }
 }
